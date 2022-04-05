c12c2f8b5af0dac48d25d121b69d2d8daf260add
Alex Rothberg <agrothberg@gmail.com>
2022-04-05 13:00:08 -0400

    Initial import of our dialect + dbapi (#1)

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # sqlalchemy-apsw
 An APSW driver for the sqlite Dialect in SQLAlchemy
+
+Code originally copied from https://github.com/rogerbinns/apsw and then Virtual Table logic stripped out.
+
+## Guide
+See: https://github.com/sqlalchemy/sqlalchemy/blob/main/README.dialects.rst
+
+## Example
+```python
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite+apsw:///')
+```
---
+++ apsw_dbapi/dialect.py
@@ -0,0 +1,67 @@
+# pylint: disable=protected-access, abstract-method
+"""A SQLALchemy dialect."""
+from typing import Any, Dict, Optional, Tuple
+
+from sqlalchemy.dialects.sqlite.base import SQLiteDialect
+from sqlalchemy.engine.url import URL
+from sqlalchemy.sql.type_api import TypeEngine
+from sqlalchemy.sql.visitors import VisitableType
+from typing_extensions import TypedDict
+
+from . import db
+
+
+class SQLAlchemyColumn(TypedDict):
+    """
+    A custom type for a SQLAlchemy column.
+    """
+
+    name: str
+    type: VisitableType
+    nullable: bool
+    default: Optional[str]
+    autoincrement: str
+    primary_key: int
+
+
+class APSWDialect(SQLiteDialect):
+
+    """
+    A SQLAlchemy dialect for for sqlite that uses APSW.
+
+    The dialect is based on the ``SQLiteDialect``, since we're using APSW.
+    """
+
+    name = "sqlite-apsw"
+    driver = "apsw"
+
+    # This is supported in ``SQLiteDialect``, and equally supported here. See
+    # https://docs.sqlalchemy.org/en/14/core/connections.html#caching-for-third-party-dialects
+    # for more context.
+    supports_statement_cache = True
+
+    # The base class:
+    # https://github.com/sqlalchemy/sqlalchemy/blob/main/lib/sqlalchemy/dialects/sqlite/base.py
+
+    # TODO(cancan101): figure out if this is true
+    # ``SQLiteDialect.colspecs`` has custom representations for objects that SQLite stores
+    # as string (eg, timestamps). Since the our DB API driver returns them as
+    # proper objects the custom representations are not needed.
+    colspecs: Dict[TypeEngine, TypeEngine] = {}
+
+    @classmethod
+    def dbapi(cls):  # pylint: disable=method-hidden
+        """
+        Return the DB API module.
+        """
+        return db
+
+    def create_connect_args(
+        self,
+        url: URL,
+    ) -> Tuple[Tuple[()], Dict[str, Any]]:
+        path = str(url.database) if url.database else ":memory:"
+        return (), {
+            "path": path,
+            "isolation_level": self.isolation_level,
+        }
